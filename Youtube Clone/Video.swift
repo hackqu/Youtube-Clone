2727afacc8266593a71350a6279769943080cc22
hackqu <tsaqifammar.jr@gmail.com>
2021-03-15 11:29:35 +0700

    Day 1 = setting Youtube API dan memanggil datanya ke ViewContrroler

---
+++ Youtube Clone/Video.swift
@@ -0,0 +1,16 @@
+//
+//  Video.swift
+//  Youtube Clone
+//
+//  Created by tsaqifammar on 15/03/21.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
