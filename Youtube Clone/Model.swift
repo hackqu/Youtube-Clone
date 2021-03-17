5e1c83a03a52c913891b66e3f9143d3b33ec4265
hackqu <tsaqifammar.jr@gmail.com>
2021-03-17 13:23:58 +0700

    Hari ke 3

--- Youtube Clone/Model.swift
+++ Youtube Clone/Model.swift
@@ -4,27 +4,56 @@
 //
 //  Created by tsaqifammar on 15/03/21.
 //
-
 import Foundation
+
+protocol ModelDelegate {
+    
+    func videosFetched(_ videos:[Video])
+    
+}
 
 class Model {
     
-    func getVideos() {
-        let url = URL(string: Constants.API_URL)
+    var delegate: ModelDelegate?
+  //buat fungsi untuk ngambil data dari Youtube API
+  func getVideos() {
+    // simpan url ke dalam variabel
+    let url = URL (string: Constants.API_URL)
+    
+    // cek urlnya kosong apa engga?
+    guard url != nil else {
+      return
+    }
+    // Mendapatkan URLSession dari object
+    let session = URLSession.shared
+    
+    // mendapatkan data dari URLSession
+    let dataTask = session.dataTask(with: url!) { (data, response, error) in
+      // cek apakah ada error?
+      if error != nil || data == nil {
+        return
+      }
+      
+      do {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
         
-        guard url != nil else{
-            return
-        }
+        let response = try decoder.decode(Response.self, from: data!)
         
-        let session = URLSession.shared
-        
-        let dataTask = session.dataTask(with: url!) { ( data, response, error) in
-            
-            if error != nil || data == nil {
-                return
+        if response.items != nil {
+            DispatchQueue.main.async {
+                self.delegate?.videosFetched(response.items!)
             }
         }
         
-        dataTask.resume()
+        dump(response)
+      }
+      catch{
+        
+      }
+      
     }
+    // melanjutkan Task
+    dataTask.resume()
+  }
 }
